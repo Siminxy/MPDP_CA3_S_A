d931e624910b424f23aed15b3506820c687f5329
Siminxy <D00219638@student.dkit.ie>
2022-05-14 13:00:32 +0100

    Limit added to player names
    -Player names now initializes by substring of inputted name up to 10 chars.

--- RoboCatSFMLServer/ClientProxy.cpp
+++ RoboCatSFMLServer/ClientProxy.cpp
@@ -7,7 +7,7 @@ namespace
 
 ClientProxy::ClientProxy(const SocketAddress& inSocketAddress, const string& inName, int inPlayerId) : 
 	mSocketAddress(inSocketAddress),
-	mName(inName),
+	mName(inName.substr(0, 10)),
 	mPlayerId(inPlayerId),
 	mDeliveryNotificationManager(false, true),
 	mIsLastMoveTimestampDirty(false),
